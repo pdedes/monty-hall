3b5f7616c01da9e02231918a07502ef499685086
pdedes <dedes1821@gmail.com>
2015-01-23 22:04:35 -0500

    Started defining the program.

---
+++ montyHall.js
@@ -0,0 +1,21 @@
+var montyHall = function (rounds) {
+	var stayWins = 0;
+	var switchWins = 0;
+	var doors = [];
+	var player = [];
+
+	var chooseDoor = function () {
+		return Math.floor(Math.random()*3) + 1;
+	}
+
+	for (var i = 0; i < rounds; i++) {
+		//prize is placed
+		//user makes decision
+		//one non-prize door is revealed
+		//user makes choice
+		//game result counted
+	}
+
+
+
+}
