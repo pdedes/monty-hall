b2e3ee32bf3148eea5cdefc378095e0641865fc4
pdedes <dedes1821@gmail.com>
2015-01-23 22:57:57 -0500

    Added mechanicsm for determining when a game is won (rudimentary).

--- montyHall.js
+++ montyHall.js
@@ -13,17 +13,22 @@ var montyHall = function (rounds) {
 	// }
 
 	for (var i = 0; i < rounds; i++) {
-		
+		doors[chooseDoor()] = true;
+		player[chooseDoor()] = true;
+		var j = 0;
 
-		doors[chooseDoor()] = "prize";
-		player[chooseDoor()] = "choice";
+		while( j < 3 ){
+			if (doors[j] && player[j]) {
+				console.log(doors[j]);
+				console.log(player[j]);
+				console.log("winner!");
+			}
+			j++;
+		}
 		//one non-prize door is revealed
 		//user makes choice to stay or switch
 		//game result counted
 	}
-
-	console.log(doors);
-	console.log(player);
 
 }
 
