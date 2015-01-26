27475be0d4ec86f4c22c6497e8da46bd01394da3
pdedes <dedes1821@gmail.com>
2015-01-26 18:39:38 -0500

    Adjusting strategy. Creating a game prototype that can be instantiated and includes prototype functions.

--- montyHall.js
+++ montyHall.js
@@ -1,16 +1,15 @@
-var montyHall = function (rounds) {
-	var stayWins = 0;
-	var switchWins = 0;
-	var doors = [];
-	var player = [];
+function MontyHall () {
+	this.door = undefined;		//Will hold 1, 2, or 3 signifying a placeholder for the prize
+	this.player = undefined;	//Will hold 1, 2, or 3 signifying the player's current choice
+}
 
-	var chooseDoor = function () {
-		return Math.floor(Math.random()*3) + 1;
-	}
+MontyHall.prototype.chooseDoor = function () {
+	return Math.floor(Math.random()*3) + 1;
+}
 
-	// var revealDoor = function () {
-
-	// }
+var runSimulation = function (rounds) {
+	var switchWins = undefined;
+	var stayWins = undefined;
 
 	for (var i = 0; i < rounds; i++) {
 		doors[chooseDoor()] = true;
@@ -19,8 +18,6 @@ var montyHall = function (rounds) {
 
 		while( j < 3 ){
 			if (doors[j] && player[j]) {
-				console.log(doors[j]);
-				console.log(player[j]);
 				console.log("winner!");
 			}
 			j++;
@@ -29,6 +26,4 @@ var montyHall = function (rounds) {
 		//user makes choice to stay or switch
 		//game result counted
 	}
-
 }
-
