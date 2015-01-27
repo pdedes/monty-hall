2d0e163b2ff5dbfe080f3fcf2260ba7a3c80481f
pdedes <dedes1821@gmail.com>
2015-01-26 20:32:25 -0500

    Refactored some if/else blocks to ternary operators. I'm choosing not to reduce the prototype.switch and prototype.stay methods into one another because I think it will muddy the understanding of each.

--- montyHall.js
+++ montyHall.js
@@ -8,26 +8,21 @@ MontyHall.prototype.chooseDoor = function () {
 	return Math.floor(Math.random()*3) + 1;
 }
 
+//This function plays the aggressive strategy. The do/while loop ensures that the player's choice is changed.
 MontyHall.prototype.switch = function (door, player, reveal) {
 	var original = player;
 
+	//The player will have to pick a door that is neither the one he is currently on, nor the door revealed
 	do {
 		player = this.chooseDoor();
 	} while (player === reveal || player === original);
 
-	if(player === door){
-		return true;
-	} else {
-		return false;
-	}
+	return (player === door) ? true : false;
 }
 
+//This prototype function plays the conservative strategy
 MontyHall.prototype.stay = function (door, player) {
-	if(player === door){
-		return true;
-	} else {
-		return false;
-	}
+	return (player === door) ? true : false;
 }
 
 var runSimulation = function (rounds) {
@@ -44,17 +39,18 @@ var runSimulation = function (rounds) {
 			gameInstance.reveal = gameInstance.chooseDoor();
 		} while (gameInstance.reveal === gameInstance.door || gameInstance.reveal === gameInstance.player);
 
-		console.log("door value:   " + gameInstance.door);
-		console.log("player value: " + gameInstance.player);
-		console.log("monty reveal: " + gameInstance.reveal);
+		// Console logging for debugging purposes
+		// console.log("door value:   " + gameInstance.door);
+		// console.log("player value: " + gameInstance.player);
+		// console.log("monty reveal: " + gameInstance.reveal);
 		
-		// debugger;
 		if (gameInstance.stay(gameInstance.door, gameInstance.player)) {stayWins++;}
 		if (gameInstance.switch(gameInstance.door, gameInstance.player, gameInstance.reveal)) {switchWins++;}
-		console.log("---");
+		// console.log("---");
 
 	}
 
+	//Probability is calculated using the wins divided by the number of rounds
 	console.log("P(Never Switching)  = " + stayWins/rounds);
 	console.log("P(Always Switching) = " + switchWins/rounds);
 }
