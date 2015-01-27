d2d8a7740b386eda552e8b4cd0a926ac0b44af19
pdedes <dedes1821@gmail.com>
2015-01-26 19:01:24 -0500

    Added logic for initial and player door choices. Also added Monty's door reveal logic.

--- montyHall.js
+++ montyHall.js
@@ -1,6 +1,7 @@
 function MontyHall () {
 	this.door = undefined;		//Will hold 1, 2, or 3 signifying a placeholder for the prize
 	this.player = undefined;	//Will hold 1, 2, or 3 signifying the player's current choice
+	this.reveal = undefined;
 }
 
 MontyHall.prototype.chooseDoor = function () {
@@ -12,16 +13,23 @@ var runSimulation = function (rounds) {
 	var stayWins = undefined;
 
 	for (var i = 0; i < rounds; i++) {
-		doors[chooseDoor()] = true;
-		player[chooseDoor()] = true;
-		var j = 0;
+		var gameInstance = new MontyHall();
 
-		while( j < 3 ){
-			if (doors[j] && player[j]) {
-				console.log("winner!");
-			}
-			j++;
-		}
+		gameInstance.door = gameInstance.chooseDoor();
+		gameInstance.player = gameInstance.chooseDoor();
+
+		do {
+			gameInstance.reveal = gameInstance.chooseDoor();
+		} while (gameInstance.reveal === gameInstance.door || gameInstance.reveal === gameInstance.player);
+
+		console.log("door value:   " + gameInstance.door);
+		console.log("player value: " + gameInstance.player);
+		console.log("monty reveal: " + gameInstance.reveal);
+		console.log("---");
+		// if (gameInstance.door && gameInstance.player) {
+		// 	console.log("winner!");
+		// }
+			
 		//one non-prize door is revealed
 		//user makes choice to stay or switch
 		//game result counted
